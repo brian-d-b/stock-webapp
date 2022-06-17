fd5c0ee8ca3a5943cd8893466bbe570a49db343c
caffeineboy <brian.d.belsito@gmail.com>
2022-06-17 16:20:15 -0400

    update tradingview section

--- src/sections/tradingview-indicator.tsx
+++ src/sections/tradingview-indicator.tsx
@@ -1,5 +1,5 @@
-import { useColorModeValue, List, ListItem, ListIcon, Box, Center, SimpleGrid, GridItem, useColorMode, Heading, Text, Button, Select, Container, Flex, VStack } from "@chakra-ui/react"
-import { CheckCircleIcon, SettingsIcon, ArrowUpIcon, ArrowForwardIcon, ArrowDownIcon, UpDownIcon } from '@chakra-ui/icons'
+import { useColorModeValue, Link, List, ListItem, ListIcon, Box, Center, SimpleGrid, GridItem, useColorMode, Heading, Text, Button, Select, Container, Flex, VStack } from "@chakra-ui/react"
+import { CheckCircleIcon, SettingsIcon, InfoIcon, ArrowUpIcon, ArrowForwardIcon, ArrowDownIcon, UpDownIcon } from '@chakra-ui/icons'
 import React, { useState, useEffect } from "react";
 
 
@@ -20,7 +20,7 @@ const TVIndicators = () => {
     >
 
 <Center>
-<Center><Heading>TradingView Indicators by sector</Heading></Center></Center>
+<Center><Heading>TradingView Sectors Indicator</Heading></Center></Center>
 
 <iframe height='300px' width='600px' src="https://docs.google.com/spreadsheets/d/e/2PACX-1vSxN0QEcFxIxxwV4kv1wCaz7wMUScJo4bfz2S_4AkLY0Ixp3OfePp_--6DjRX5xt6Sdl3k7CQVtXCK6/pubhtml?gid=0&amp;single=true&amp;widget=true&amp;headers=false"></iframe>
 
@@ -28,7 +28,15 @@ const TVIndicators = () => {
     <ListItem><Text padding='15px'><ListIcon as={CheckCircleIcon} color='green.500'></ListIcon>
     This public sheet is automatically updated via a Google Cloud Function that takes incoming POST requests and formats them into this sheet.
     </Text></ListItem>
+    <ListItem><Text padding='15px'><ListIcon as={InfoIcon}></ListIcon>
+      This indicator receives Buy/Sell signals on the 1D Interval
+    </Text></ListItem>
+    <ListItem><Text padding='15px'><ListIcon as={InfoIcon}></ListIcon>
+      <Link color='teal.500' href='https://www.tradingview.com/script/Msm4SjwI-VuManChu-Cipher-B-Divergences/'>Find out more about this indicator.</Link>
+    </Text></ListItem>
 </List></Center>
+
+<Center><Text fontSize='sm'>Nothing on this website is financial advice!</Text></Center>
 
 </VStack>
 
